df9152acdbb62a4943b8afe7d29de84553fa7bdd
robwatsongtr <rwatso@gmail.com>
2024-03-22 22:06:45 -0700

    binary tree

---
+++ binary_tree_with_sll/binary_tree.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "singly_linked_list_bin_tree.h"
+#include <stdlib.h>
+
+int main() {
+    struct TreeNodeChar* root = createTreeNodeChar("root");
+    struct TreeNodeChar* node2 = createTreeNodeChar("node_2");
+    struct TreeNodeChar* node3 = createTreeNodeChar("node_3");
+    struct TreeNodeChar* node4 = createTreeNodeChar("node_4");
+    struct TreeNodeChar* node5 = createTreeNodeChar("node_5");
+    struct TreeNodeChar* node6 = createTreeNodeChar("node_6");
+    struct TreeNodeChar* node7 = createTreeNodeChar("node_7");
+
+    root->left = node2;
+    root->right = node3;
+    node2->left = node4;
+    node2->right = node5; 
+    node3->left = node6;
+    node3->right = node7;
+
+}
